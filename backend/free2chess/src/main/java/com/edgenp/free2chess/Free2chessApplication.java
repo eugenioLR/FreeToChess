36436b55bfeb88acaea1bec5502bceca8c733ff3
eugeniolr <eugeniolorente0@gmail.com>
2022-01-21 18:05:29 +0100

    Classes created

---
+++ backend/free2chess/src/main/java/com/edgenp/free2chess/Free2chessApplication.java
@@ -0,0 +1,13 @@
+package com.edgenp.free2chess;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Free2chessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Free2chessApplication.class, args);
+	}
+
+}
---
+++ backend/free2chess/src/test/java/com/edgenp/free2chess/Free2chessApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edgenp.free2chess;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Free2chessApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
