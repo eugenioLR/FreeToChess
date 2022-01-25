71903ebd7bc01c2b61152e64bd3d48c6af7ef376
eugeniolr <eugeniolorente0@gmail.com>
2022-01-25 14:11:15 +0100

    misterious NAN bug solved

--- web/script.js
+++ web/script.js
@@ -2,7 +2,7 @@
 
 const xhr = new XMLHttpRequest();
 
-function insertProductPack() 
+function insertProductPack()
 {
     xhr.onreadystatechange = function()
     {
@@ -13,16 +13,17 @@ function insertProductPack()
             var array_json = JSON.parse(string_json);
             for (var i = 0; i < array_json.length ; i++)
             {
-                e[0].insertAdjacentHTML('afterbegin','<div class="productS">' +
-                                        + '<form action="">' 
+                console.log(array_json[i])
+                e[0].insertAdjacentHTML('afterbegin','<div class="product' + array_json[i].rarity + '">'
+                                        + '<form action="">'
                                         + '<button type="button" class="purchasebutton">Purchase</button>'
                                         + '</form>'
-                                        + '<p class="producttitle">' + String(array_json[i].name) + '</p>'
-                                        + '<img src="img/lootboxes/'+ String(array_json[i].rarity) +'.png" class="producticon">'
-                                        + '<p class="description">' + String(array_json[i].description) + '</p>'
-                                        + '<p class="price">' + parseFloat(array_json[i].price) + '<img src="/img/diamond.png" class="diamond"></p>'
-                                        + '<p class="discount">' + parseFloat(array_json[i].discount_perc * 100) + '%</p>'
-                                        + '<p class="rarity">' + String(array_json[i].rarity) + '</p>'
+                                        + '<p class="producttitle">' + array_json[i].name + '</p>'
+                                        + '<img src="img/lootboxes/'+ array_json[i].rarity +'.png" class="producticon">'
+                                        + '<p class="description">' + array_json[i].description + '</p>'
+                                        + '<p class="price">' + array_json[i].price + '<img src="/img/diamond.png" class="diamond"></p>'
+                                        + '<p class="discount">' + array_json[i].discount_perc * 100 + '%</p>'
+                                        + '<p class="rarity">' + array_json[i].rarity + '</p>'
                                         + '</div>');
             }
         }
@@ -32,17 +33,17 @@ function insertProductPack()
 }
 
 
-// xhr.onreadystatechange = function() 
+// xhr.onreadystatechange = function()
 // {
-//     if (xhr.readyState == 4 && xhr.status == 200) 
+//     if (xhr.readyState == 4 && xhr.status == 200)
 //     {
 //         var string_json = xhr.responseText;
 //         var array_json = JSON.parse(string_json);
-//         for (var i = 0 ; i < array_json.length; i++) 
+//         for (var i = 0 ; i < array_json.length; i++)
 //         {
 //             console.log(array_json[i].name);
 //         }
-//     }    
+//     }
 // }
 
 // xhr.open("get", "http://localhost:8080/store", true);
